bbd8d90b9b232c1feebd9126ec3a8152610a939e
Alex Yiu <alex13920.cep@gmail.com>
2020-10-31 12:57:50 +0800

    initial code

---
+++ stocks/quotes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Stock (models.Model): 
+    ticker= models.CharField(max_length= 8)
+
+    def __str__(self):
+        return self.ticker
+
+
